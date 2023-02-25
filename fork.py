6becfd379811a9bd901e745cc8e2b67583f5c356
petruha <refloader@gmail.com>
2023-02-25 07:35:49 +0300

    Modify fork.py

--- fork.py
+++ fork.py
@@ -1,34 +1,58 @@
 import os, time
 import sys
-
-print('Hello! I am an example')
-pid = os.fork()
-print('pid of my child is %s' % pid)
-if pid == 0:
-    print('I am a child. Im going to sleep')
-    for i in range(1,40):
-      print('mrrrrr')
-      a = 2**i
-      print(a)
-      pid = os.fork()
-      if pid == 0:
-            print('my name is %s' % a)
-            sys.exit(0)
-      else:
-            print("my child pid is %s" % pid)
-      time.sleep(1)
-    print('Bye')
-    sys.exit(0)
-
-else:
-    for i in range(1,200):
-      print('HHHrrrrr')
-
-      time.sleep(1)
-      print(3**i)
-    print('I am the parent')
-
-#pid, status = os.waitpid(pid, 0)
-#print "wait returned, pid = %d, status = %d" % (pid, status)
+import random
 
 
+def log(text):
+    print(f"[{os.getpid()}] {text}")
+
+
+def impossible_job(a):
+    time.sleep(a % 10)
+    return random.randint(0, 1)
+
+
+def to_int(int16):
+    return int(int16 / 256)
+
+
+if __name__ == "__main__":
+    log("Здрасьте! Я начальник. Слышал, что в линукс курсе нужно обновить методички.")
+    log("Кто там главный по курсу, давайте его позовем!")
+
+    pid = os.fork()
+
+    if pid == 0:
+        teachers_count = 40
+        teachers_pids = []
+        log(
+            f"Я руководитель курса. Пусть все {teachers_count} преподавателей сами разберуться"
+        )
+        for i in range(1, teachers_count):
+            a = 2**i
+            log(f"Эй, нужно обновить методичку #{a}")
+            pid = os.fork()
+
+            if pid == 0:
+                log(f"Эх, мне досталась самая сложная методичка #{a}")
+                result = impossible_job(a)
+                log(f"Готово, {'почти ' if result==1 else ''}получилось!")
+                sys.exit(result)
+
+            teachers_pids.append(pid)
+            log(f"Так-с, {pid}`у выдали работенку..")
+
+        log("Вздремнем, пока они там работают. Zzz..zz..zz")
+
+        res = 0
+        for tpid in teachers_pids:
+            _, status = os.waitpid(tpid, 0)
+            res += 0 if status == os.EX_OK else 1
+
+        sys.exit(res)
+
+    log(f"PID руководителя {pid}, подожду, пока он там со всем разберется.")
+    pid, status = os.waitpid(pid, 0)
+    log(
+        f"Руководитель курса доложил, что не получилось обновить {to_int(status)} методичек"
+    )
